641a93a368b3a44598ff92085443a048f2eab442
Aditya-Singh2008 <96719766+Aditya-Singh2008@users.noreply.github.com>
2022-05-08 07:08:39 +0000

    Update dsymec.h
    Added file support and many other functionalities

--- SymmEncryption/dsymec.h
+++ SymmEncryption/dsymec.h
@@ -1,32 +1,64 @@
 int inputlen();
+int filewrite(char *filesname, int * pta, int charlen);
 void getkey();
 void fill(int * ptr);
 void shiftrow1(int * ptr, int modtotal);
 void shiftrow2(int * pta, int modtotal);
 void xor1(int * pta, int modtotal);
 void xor2(int * pta, int modtotal);
+void textout(int * pta, int total);
 
-int tempascii[2000] = {0};
-int keyexpand[4] = {19, 21, 24, 27};
+int keyexpand[4] = {2, 3, 6, 9};
 int key[4] = {0};
 int keyxored[4] = {0};
+int charlen;
+int * tempascii = NULL;
 int * ptrascii = NULL;
 
+int inputfilelen(char *filesname){
+  FILE * readfile = fopen(filesname, "r");
+  int input;
+  int sum = 0;
+
+  if(readfile == NULL){
+    printf("Enter a valid file name");
+    exit(1);
+  }
+
+  while((input = fgetc(readfile)) != EOF){
+    *(tempascii + sum) = input;
+    sum++;
+  }
+  fclose(readfile);
+  return sum;
+}
+
+int filewrite(char *filesname, int * pta, int charlen){
+  FILE * writefile = fopen(filesname, "w");
+  if(writefile == NULL){
+    printf("Enter a valid file name");
+    exit(1);
+  }
+  int input;
+  for(int element = 0; element < charlen; element++){
+    fputc(*(pta + element), writefile);
+  }
+  fclose(writefile);
+}
 
 int inputlen(){
-  printf(" [Enter Input]: ");
+  printf("[Enter Input]: ");
   int input;
   int sum = 0;
   while((input = getchar()) != EOF){
-    tempascii[sum] = input;
+    *(tempascii + sum) = input;
     sum++;
   }
   return sum;
 }
 
-
 void getkey(){
-  printf(" [Enter Key]: ");
+  printf("[Enter Key]: ");
   int keyinput;
   int element = 0;
   int expendno = 0;
@@ -43,8 +75,8 @@ void getkey(){
 }
 
 void fill(int * pta){
-  for(int elementno = 0; tempascii[elementno] != 0; elementno++){
-    *(pta + elementno) = tempascii[elementno];
+  for(int elementno = 0; *(tempascii + elementno) != 0; elementno++){
+    *(pta + elementno) = *(tempascii + elementno);
   }
 }
 
@@ -52,7 +84,7 @@ void shiftrow1(int * pta, int modtotal){
   int swhicher;
   int group = 0;
 
-  while(group <= modtotal){
+  while(group < modtotal){
     int element1 = group + 1;
     int element2 = group + 2;
     swhicher = *(pta + element1);
@@ -77,29 +109,29 @@ void shiftrow2(int * pta, int modtotal){
 }
 
 void xor1(int * pta, int modtotal){
-  int swhicher;
   int element = 0;
   int keyelement = 0;
 
   while(element < modtotal){
     *(pta + element) = *(pta + element) ^ key[keyelement];
     element++;
-    if(element % 4 == 0){
-      keyelement++;
+    keyelement++;
+    if(keyelement == 4){
+      keyelement = 0;
     }
   }
 }
 
 void xor2(int * pta, int modtotal){
-  int swhicher;
   int element = 0;
   int keyelement = 0;
 
   while(element < modtotal){
     *(pta + element) = *(pta + element) ^ keyxored[keyelement];
     element++;
-    if(element % 4 == 0){
-      keyelement++;
+    keyelement++;
+    if(keyelement == 4){
+      keyelement = 0;
     }
   }
 }
